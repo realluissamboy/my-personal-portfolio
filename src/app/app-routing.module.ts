5f395c5f250d741919aa7ce594a00023e6d6ce72
Luis Samboy <luissamboy@icloud.com>
2023-10-14 14:40:47 -0400

    Fixed routing for home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AboutComponent } from './about/about.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'portfolio', component: PortfolioComponent },
 ];
